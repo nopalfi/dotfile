d207bba8a81ec6cb08ee7db15452f85cb7994a62
nopalfi <nopalfahriislami@yahoo.com>
2021-10-08 07:05:33 +0800

    added spectrwm dotfiles

---
+++ spectrwm/baraction.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# baraction.sh for spectrwm status bar
+# Copied from DT baraction.sh dotfiles in GitLab
+
+
+## RAM
+mem() {
+  mem=`free | awk '/Mem/ {printf "%dM/%dM\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e "$mem"
+}
+
+## CPU
+cpu() {
+  read cpu a b c previdle rest < /proc/stat
+  prevtotal=$((a+b+c+previdle))
+  sleep 0.5
+  read cpu a b c idle rest < /proc/stat
+  total=$((a+b+c+idle))
+  cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
+  echo -e "CPU: $cpu%"
+}
+
+## VOLUME
+vol() {
+    vol=`amixer get Master | awk -F'[][]' 'END{ print $4":"$2 }' | sed 's/on://g'`
+    echo -e "VOL: $vol"
+}
+
+SLEEP_SEC=3
+#loops forever outputting a line every SLEEP_SEC secs
+
+# It seems that we are limited to how many characters can be displayed via
+# the baraction script output. And the the markup tags count in that limit.
+# So I would love to add more functions to this script but it makes the 
+# echo output too long to display correctly.
+while :; do
+    echo "+@fg=2; $(cpu) | +@fg=2;  $(mem) +@fg=2; | +@fg=2;  $(vol) +@fg=2; |"
+	sleep $SLEEP_SEC
+done
+
